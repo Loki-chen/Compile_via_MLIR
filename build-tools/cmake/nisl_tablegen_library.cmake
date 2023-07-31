44a4880eb31826eb808683d1842fc0d5a1753d3f
chenyuntian <renjw02@foxmail.com>
2023-07-31 20:20:03 +0800

    cmake tools: add Targetbackend mlir config

--- build-tools/cmake/nisl_tablegen_library.cmake
+++ build-tools/cmake/nisl_tablegen_library.cmake
@@ -1,6 +1,4 @@
-# Licensed under the Apache License v2.0 with LLVM Exceptions.
-# See https://llvm.org/LICENSE.txt for license information.
-# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
 
 include(CMakeParseArguments)
 
@@ -18,47 +16,53 @@ function(nisl_tablegen_library)
     )
 
   set(_NAME "${_RULE_NAME}")
+
   if(${_RULE_TBLGEN} MATCHES "NISL")
     set(_TBLGEN "NISL")
   else()
     set(_TBLGEN "MLIR")
   endif()
 
+
   set(LLVM_TARGET_DEFINITIONS ${_RULE_TD_FILE})
+
   set(_INCLUDE_DIRS
     "${MLIR_INCLUDE_DIRS}"
-    "${NISL_SOURCE_DIR}/compiler"
-    "${NISL_BINARY_DIR}/compiler"
+    "${NISL_SOURCE_DIR}/compiler/src"
+    "${NISL_BINARY_DIR}/compiler/src"
   )
-  if(DEFINED NISL_COMPILER_TABLEGEN_INCLUDE_DIRS)
-    list(APPEND _INCLUDE_DIRS ${NISL_COMPILER_TABLEGEN_INCLUDE_DIRS})
-  endif()
   list(APPEND _INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR})
+  list(TRANSFORM _INCLUDE_DIRS PREPEND "-I")
+
   
   set(_OUTPUTS)
-  while(_RULE_OUTS)
-    # Eat any number of flags (--a --b ...) and a single file.
-    # Flags only impact the successively declared file.
-    set(_COMMAND)
-    set(_FILE)
-    while(_RULE_OUTS AND NOT _FILE)
-      list(GET _RULE_OUTS 0 _PART)
-      list(REMOVE_AT _RULE_OUTS 0)
-      if(${_PART} MATCHES "^-.*")
-        # Flag (- or --).
-        list(APPEND _COMMAND ${_PART})
-      else()
-        # File path.
-        set(_FILE ${_PART})
-      endif()
-    endwhile()
-    tablegen(${_TBLGEN} ${_FILE} ${_COMMAND} ${_INCLUDE_DIRS})
-    list(APPEND _OUTPUTS ${CMAKE_CURRENT_BINARY_DIR}/${_FILE})
-   endwhile()
 
-   add_custom_target(${_NAME}_target DEPENDS ${_OUTPUTS})
-   set_target_properties(${_NAME}_target PROPERTIES FOLDER "Tablegenning")
+  list(GET _RULE_OUTS 0 _COMMAND)
+
+
+  while(_RULE_OUTS AND NOT _FILE)
+  list(GET _RULE_OUTS 0 _PART)
+  list(REMOVE_AT _RULE_OUTS 0)
+  if(${_PART} MATCHES "^-.*")
+    # Flag (- or --).
+    list(APPEND _COMMAND ${_PART})
+  else()
+    # File path.
+    set(_FILE ${_PART})
+  endif()
+  tablegen(${_TBLGEN} ${_FILE} ${_COMMAND} ${_INCLUDE_DIRS})
+  list(APPEND _OUTPUTS ${CMAKE_CURRENT_BINARY_DIR}/${_FILE})
+  endwhile()
+
+
+  add_custom_target(${_NAME}_target DEPENDS ${_OUTPUTS})
+  set_target_properties(${_NAME}_target PROPERTIES FOLDER "Tablegenning")
  
-   add_library(${_NAME} INTERFACE)
-   add_dependencies(${_NAME} ${_NAME}_target)
+  add_library(${_NAME} INTERFACE)
+  add_dependencies(${_NAME} ${_NAME}_target)
 endfunction(nisl_tablegen_library)
+
+
+# set(LLVM_TARGET_DEFINITIONS InstCombineTables.td)
+# tablegen(LLVM InstCombineTables.inc -gen-searchable-tables)
+# add_public_tablegen_target(InstCombineTableGen)
